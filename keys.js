3dc18190545d16e51e02d50edcb6a172dd339e84
Octavia Cephas <octavia.cephas@gmail.com>
2018-02-20 19:32:29 -0500

    twitter functionality

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.a5LAz5DbyYrHjPHJnR6qxQOmt,
+  consumer_secret: process.env.RmSmWRL2ypUHNfHTvT6eHiyFBI3A1dnSeeQL4YWTFfaaAZBEYO,
+  access_token_key: process.env.962760280181702656-fY9MPGf9nLNDn87fGZAdAC3FAaXW0zi,
+  access_token_secret: process.env.9pXm1aDqN8EXG16AbJv7wiHQ6ryh5TVqxKRjFdPj8JBS7
+
 };
 
 exports.spotify = {
--- liri.js
+++ liri.js
@@ -1,2 +1,60 @@
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
+// Setting .env
+
+require("dotenv").config();
+
+
+
+
+
+///Importing dependencies and NPM packages 
+
+const Spotify = require("node-spotify-api");
+const Twitter = require("twitter");
+const keys = require("./keys");
+const fs = require("fs");
+const request = require("request");
+
+
+const spotify = new Spotify(keys.spotify);
+
+//creating search for tweets 
+
+const generateTweets = function() {
+	const client = new Twitter(keys.twitter);
+
+	const info = {
+		handle: "wigqueen_"
+	};
+	client.get("statuses/user_timeline", info function(error, tweets, response) {
+		if (!error) {
+			for (let i = 0; i <tweets.length; i++) {
+				console.log(tweets[i].created_at);
+				console.log("Here's a rundown of your latest tweets:");
+				console.log(tweets[i].text);
+			}
+		}
+
+	});
+};
+
+// request("https://twitter.com/WigQueen_?t=", function(error, response, body){
+
+// iff(!error && response.statusCode === 200) {
+
+// 	console.log("Your last 20 Tweets were:" + JSON.parse(body).)
+// }
+// })
+
+
+
+
+
+// * `my-tweets`
+
+//     * `spotify-this-song`
+
+//     * `movie-this`
+
+//     * `do-what-it-says
+
