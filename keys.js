36a12ea939845e11a4d58ceafbb917e512ff5d81
Octavia Cephas <octavia.cephas@gmail.com>
2018-02-22 21:07:01 -0500

    keys.js, .env

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.a5LAz5DbyYrHjPHJnR6qxQOmt,
-  consumer_secret: process.env.RmSmWRL2ypUHNfHTvT6eHiyFBI3A1dnSeeQL4YWTFfaaAZBEYO,
-  access_token_key: process.env.fY9MPGf9nLNDn87fGZAdAC3FAaXW0zi,
-  access_token_secret: process.env.pXm1aDqN8EXG16AbJv7wiHQ6ryh5TVqxKRjFdPj8JBS7
+  consumer_key: a5LAz5DbyYrHjPHJnR6qxQOmt,
+  consumer_secret: env.RmSmWRL2ypUHNfHTvT6eHiyFBI3A1dnSeeQL4YWTFfaaAZBEYO,
+  access_token_key: 962760280181702656-fY9MPGf9nLNDn87fGZAdAC3FAaXW0zi,
+  access_token_secret: 9pXm1aDqN8EXG16AbJv7wiHQ6ryh5TVqxKRjFdPj8JBS7
 
 };
 
 exports.spotify = {
-  spotify_id: process.env.31e09a43eead4e0da86e2b79b37199ca,
-  spotify_secret: process.env.96d8f262c3f246ffa570c9ffc58214b3
+  id: 31e09a43eead4e0da86e2b79b37199ca,
+  secret: 96d8f262c3f246ffa570c9ffc58214b3
 };
